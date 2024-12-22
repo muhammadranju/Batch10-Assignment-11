e2d6ea5964576d571571150560f8f599074e86e4
Muhammad Ranju <mdranju23@gmail.com>
2024-12-22 12:44:17 +0600

    feat: added auth provider that control user authentication

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import { onAuthStateChanged, signOut as logOut } from "firebase/auth";
+import { auth } from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null); // User object from Firebase
+  const [refetch, setRefetch] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const updateUser = (user) => {
+    setUser(user);
+  };
+  const signOut = () => {
+    setUser(null);
+    logOut(auth);
+    setLoading(false);
+    // Cookies.remove("isLoggedIn");
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unSubscribe;
+    };
+  }, [refetch]);
+
+  const value = {
+    updateUser,
+    user,
+    loading,
+    setUser,
+    setLoading,
+    signOut,
+    setRefetch,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
