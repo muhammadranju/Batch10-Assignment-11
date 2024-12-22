24d869ae0339258821eb53e36a0495c0997574c8
Muhammad Ranju <mdranju23@gmail.com>
2024-12-22 12:42:12 +0600

    update: the private routes is now fully workable

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,5 +1,34 @@
-const PrivateRoute = () => {
-  return <div></div>;
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { RotatingLines } from "react-loader-spinner";
+import { AuthContext } from "../context/AuthProvider";
+
+/* eslint-disable react/prop-types */
+const PrivateRoutes = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading)
+    return (
+      <div className="flex justify-center items-center mt-72">
+        <RotatingLines
+          visible={true}
+          height="50"
+          width="50"
+          strokeColor="#3b82f6"
+          strokeWidth="5"
+          animationDuration="0.75"
+          ariaLabel="rotating-lines-loading"
+          wrapperStyle={{}}
+        />
+      </div>
+    );
+
+  if (!user) {
+    return <Navigate state={location.pathname} to="/login" replace={true} />;
+  }
+
+  return <div>{children}</div>;
 };
 
-export default PrivateRoute;
+export default PrivateRoutes;
